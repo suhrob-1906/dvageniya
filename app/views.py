f271fdd12f4cfcf8a1c66a6db4efc41653371472
suhrob-1906 <suxrbjon10.m.s@gmail.com>
2025-07-25 14:25:12 +0500

    rdcommit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import BookForm
+from .models import Book
 
-# Create your views here.
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_book')
+    else:
+        form = BookForm()
+    return render(request, 'add_book.html', {'form': form})
